8188697402b4717a97f2b973c8e1a8be1a10423a
Abhinav Gorle <abhinav.gorle@honeywell.com>
2020-06-17 19:12:46 +0530

    first commit

---
+++ rpi_cam.py
@@ -0,0 +1,14 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+while capture.isOpened():
+    ret, frame = capture.read()
+    if ret:
+        print("Unable to read frame")
+        break
+    cv2.imshow("window", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+capture.release()
+cv2.destroyAllWindows()
